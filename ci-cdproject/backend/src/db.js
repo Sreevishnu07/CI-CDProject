1bd25a9da796856518eed703828dc36af62b8021
Sreevishnu07 <152363871+Sreevishnu07@users.noreply.github.com>
2026-04-07 15:23:07 +0530

    Change database connection to use connection pool

--- ci-cdproject/backend/src/db.js
+++ ci-cdproject/backend/src/db.js
@@ -1,8 +1,11 @@
 import mysql from "mysql2";
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
